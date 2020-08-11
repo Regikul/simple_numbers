88a21feb07c9ef2e55889fcf5473d6f6bb00da9e
Maksim Krzhemenevskiy <regikul@gmail.com>
2020-08-12 00:28:39 +0700

    fetch numbers from a Redis queue

---
+++ apps/number_filter/src/nf_filter.erl
@@ -0,0 +1,96 @@
+-module(nf_filter).
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(nf_filter_state, {
+    redis :: pid(),
+    in_queue :: string(),
+    out_queue :: string(),
+    batch_size :: pos_integer(),
+    kick_size :: non_neg_integer(),
+    enqueued :: non_neg_integer()
+}).
+
+%%%===================================================================
+%%% Spawning and gen_server implementation
+%%%===================================================================
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, Redis} = common:start_redis(),
+    {ok, InQueue} = application:get_env(in_queue),
+    {ok, OutQueue} = application:get_env(out_queue),
+    {ok, BatchSize} = application:get_env(batch_size),
+    {ok, MinBatchSize} = application:get_env(min_batch_size),
+    State = #nf_filter_state{
+        redis = Redis,
+        in_queue = InQueue,
+        out_queue = OutQueue,
+        batch_size = BatchSize,
+        enqueued = 0,
+        kick_size = MinBatchSize
+    },
+    {ok, kick(State)}.
+
+handle_call(Request, _From, State = #nf_filter_state{}) ->
+    lager:error("unknown call ~p", [Request]),
+    {reply, no_impl, State}.
+
+handle_cast(Request, State = #nf_filter_state{}) ->
+    lager:error("unknown cast ~p", [Request]),
+    {noreply, State}.
+
+handle_info({kick}, State = #nf_filter_state{batch_size = Size,
+                                             enqueued = Enqueued,
+                                             in_queue = InQueue,
+                                             redis = Redis}) ->
+    Batch = lists:duplicate(Size - Enqueued, ["RPOP", InQueue]),
+    lists:foreach(fun(Command) -> eredis:q_async(Redis, Command) end, Batch),
+    {noreply, State};
+handle_info({response, {ok, Value}}, State) ->
+    NewState = count_accepted(State),
+    case Value of
+        undefined -> ok;
+        Binary when is_binary(Binary) -> process(Binary)
+    end,
+    {noreply, kick(NewState)};
+handle_info(Info, State) ->
+    lager:error("unknown info ~p", [Info]),
+    {noreply, State}.
+
+terminate(_Reason, _State = #nf_filter_state{}) ->
+    ok.
+
+code_change(_OldVsn, State = #nf_filter_state{}, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+process(_Binary) ->
+    ok.
+
+count_accepted(State = #nf_filter_state{enqueued = Enqueued}) ->
+    State#nf_filter_state{enqueued = Enqueued - 1}.
+
+kick(State = #nf_filter_state{batch_size = BatchSize,
+                              kick_size = KickSize,
+                              enqueued = Enqueued,
+                              in_queue = InQueue,
+                              redis = Redis}) ->
+    case Enqueued =< KickSize of
+        true ->
+            Batch = lists:duplicate(BatchSize - Enqueued, ["RPOP", InQueue]),
+            lists:foreach(fun(Command) -> eredis:q_async(Redis, Command) end, Batch),
+            State#nf_filter_state{enqueued = BatchSize};
+        false -> State
+    end.
